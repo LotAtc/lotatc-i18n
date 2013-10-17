5566741701a0b0afccf2d288e665e5ee461f2521
DArt <dart@dartsite.org>
2013-10-17 20:19:45 +0200

    Initial french translations

---
+++ lotatc_fr.ts
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>ATCView</name>
+    <message>
+        <location filename="../ATCView.cpp" line="26"/>
+        <source>(Not registered)</source>
+        <translation>(Non enregistré)</translation>
+    </message>
+</context>
+<context>
+    <name>BlockType</name>
+    <message>
+        <location filename="../block/BlockType.cpp" line="18"/>
+        <source>UNK</source>
+        <comment>Short version of unknow</comment>
+        <translation>UNK</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectDlg</name>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="25"/>
+        <source>Address:</source>
+        <translation>Adresse:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="33"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="40"/>
+        <source>Coalition:</source>
+        <translation>Coalition:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="78"/>
+        <source>Password:</source>
+        <translation>Mot de passe:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="87"/>
+        <source>Pseudo:</source>
+        <translation>Pseudo:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="96"/>
+        <source>Connect</source>
+        <translation>Connecter</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/ConnectDlg.qml" line="104"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../resources/qml/common/Dialog.qml" line="46"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>LabelEdit</name>
+    <message>
+        <location filename="../resources/qml/client/LabelEdit.qml" line="11"/>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/LabelEdit.qml" line="20"/>
+        <location filename="../resources/qml/client/LabelEdit.qml" line="38"/>
+        <source>Default</source>
+        <translation>Default</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/LabelEdit.qml" line="29"/>
+        <source>Hovered</source>
+        <translation>Survolé</translation>
+    </message>
+</context>
+<context>
+    <name>MapItem</name>
+    <message>
+        <location filename="../resources/qml/client/MapItem.qml" line="19"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+</context>
+<context>
+    <name>NewsDlg</name>
+    <message>
+        <location filename="../resources/qml/client/NewsDlg.qml" line="30"/>
+        <source>Loading...</source>
+        <translation>Chargement...</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/NewsDlg.qml" line="31"/>
+        <source>News</source>
+        <translation>Nouvelles</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/NewsDlg.qml" line="32"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/NewsDlg.qml" line="57"/>
+        <source>See on LotAtc website: </source>
+        <translation>Voir sur le site de LotAtc:</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDlg</name>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="28"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="29"/>
+        <source>Map</source>
+        <translation>Carte</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="30"/>
+        <source>Labels</source>
+        <translation>Etiquettes</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="57"/>
+        <source>Choose a language (need application restart):</source>
+        <translation>Choisir une langue (l&apos;application doit redémarrer):</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="78"/>
+        <source>Choose a map:</source>
+        <translation>Choisir une carte:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="89"/>
+        <source>Choose a unit (Key: U)</source>
+        <translation>Choisir une unité (touche U)</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="101"/>
+        <source>Brightness</source>
+        <translation>Luminosité</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="112"/>
+        <source>Contrast</source>
+        <translation>Contraste</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="130"/>
+        <source>Define Labels for friends</source>
+        <translation>Définir les étiquettes pour les alliés</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="132"/>
+        <source>Define Labels for ennemies</source>
+        <translation>Définir les étiquettes pour les ennemies</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/OptionsDlg.qml" line="136"/>
+        <source>Show background for label (Key: L)</source>
+        <translation>Afficher le fond des étiquettes (touche L)</translation>
+    </message>
+</context>
+<context>
+    <name>Unit</name>
+    <message>
+        <location filename="../Unit.cpp" line="12"/>
+        <source>Metric</source>
+        <translation>Métrique</translation>
+    </message>
+    <message>
+        <location filename="../Unit.cpp" line="13"/>
+        <source>Imperial</source>
+        <translation>Impérial</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="27"/>
+        <source>There is something wrong with your license. Please contact support.</source>
+        <translation>Problème avec votre license, contacter le support.</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="80"/>
+        <source>Disconnected</source>
+        <translation>Déconnecté</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="87"/>
+        <source>Connection and authentification OK</source>
+        <translation>Connexion et authentification OK</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="93"/>
+        <source>Connection and authentification error: </source>
+        <translation>Erreur de connexion ou d&apos;authentification:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="107"/>
+        <source>Connection error: </source>
+        <translation>Erreur de connexion:</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="187"/>
+        <source>Not connected</source>
+        <translation>Non connecté</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="296"/>
+        <source>Connect</source>
+        <translation>Connecter</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="301"/>
+        <source>Disconnect</source>
+        <translation>Déconnecter</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="307"/>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="312"/>
+        <source>News</source>
+        <translation>Nouvelles</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="317"/>
+        <source>Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="322"/>
+        <source>Circle on</source>
+        <translation>Cercles on</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="322"/>
+        <source>Circle off</source>
+        <translation>Cercles off</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="334"/>
+        <source>METRIC</source>
+        <translation>METRIQUE</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="334"/>
+        <source>IMPERIAL</source>
+        <translation>IMPERIAL</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="370"/>
+        <source>Not registered&lt;br&gt;Put you %1 in the &lt;u&gt;%2 directory&lt;/u&gt;</source>
+        <translation>Non enregistré&lt;br&gt;Mettre votre fichier %1 dans le répertoire &lt;u&gt;%2&lt;/u&gt;</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/client/main.qml" line="381"/>
+        <source>If you do not have a license key &lt;u&gt;click HERE&lt;/u&gt; to buy one!</source>
+        <translation>Si vous n&apos;avez pas de licence &lt;u&gt;Cliquer ici&lt;/u&gt; pour en acheter une!</translation>
+    </message>
+    <message>
+        <source>Not registered</source>
+        <translation type="obsolete">Non enregistré</translation>
+    </message>
+</context>
+</TS>
